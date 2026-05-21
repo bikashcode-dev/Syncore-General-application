3e3ad3a812f616d627873a7b68a28f929e95bee3
Bikash sah <15850882+bikashsah1@users.noreply.github.com>
2026-05-21 23:59:58 +0530

    Controll commit

---
+++ SpringBoot/Syncore/src/main/java/com/SynCore/Syncore/entity/User.java
@@ -0,0 +1,29 @@
+package com.SynCore.Syncore.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "User")
+@Data
+public class User {
+
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    @NonNull
+    private String username;
+    @NonNull
+    private String password;
+    private List<String> roles;
+    @DBRef
+    private List<SynCoreEntry> synCoreEntryList = new ArrayList<>();
+}
